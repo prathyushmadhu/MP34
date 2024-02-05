b4d5212c7267ad8ef162964c0a3d801d7fa99146
prathyushmadhu <prathyushmadhu@gmail.com>
2024-02-05 23:55:48 +0530

    changed init.sql

--- python/src/auth/init.sql
+++ python/src/auth/init.sql
@@ -9,6 +9,6 @@ USE auth;
 CREATE TABLE user(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL;
-    password  VARCHAR(255) 
+    password  VARCHAR(255) NOT NULL 
 );
 
